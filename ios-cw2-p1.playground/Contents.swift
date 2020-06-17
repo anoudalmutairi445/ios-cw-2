b708942dde6b24e445bcaed55dd7909306f21a3d
analmutairi23 <anoudmutairi24@gmail.com>
2020-06-17 20:44:12 +0300

    Done

---
+++ ios-cw2-p1.playground/Contents.swift
@@ -0,0 +1,16 @@
+let family = ["Anoud","Khaled" ]
+
+var evens = [0, 2, 4, 6, 8, 10, 12, 14, 16]
+
+var grades = [95.6, 94.7, 98.2]
+
+var trueFalse = [true, true, true, false]
+
+print(family[0], family[1])
+
+print(evens[0], evens[8])
+
+print(grades[0], grades[2])
+
+print(trueFalse[0], trueFalse[3])
+ 
